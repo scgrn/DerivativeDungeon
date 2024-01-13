fca0a5e79815a359bcb576fd5f9c24afed4b3e71
Andrew Krause <ajkrause@gmail.com>
2024-01-13 09:30:28 -0500

    Place keys

--- script/items.lua
+++ script/items.lua
@@ -47,6 +47,8 @@ function drawItems()
 end
 
 function placeItems()
+    placeSpellbooks()
+    
     --  place amulet
     local room = table.remove(dungeon[FLOORS].deadEnds)
 
@@ -71,7 +73,7 @@ function placeItems()
     table.insert(dungeon[lanternFloor][room.x][room.y].items, {
         x = 5,
         y = 5,
-        representation = 'l',
+        representation = 'Å',
         effect = function()
             messageBox.open({"You found a LANTERN to light your way."})
             inventory.lantern = true
@@ -154,5 +156,110 @@ function placeItems()
         end,
         permanent = true,
     })
+    
+    --  place keys
+    local floor
+    if (#dungeon[1].deadEnds == 0) then
+        floor = 2
+    elseif (#dungeon[2].deadEnds == 0) then
+        floor = 1
+    else
+        floor = random(1, 2)
+    end
+    room = table.remove(dungeon[floor].deadEnds)
+    table.insert(dungeon[floor][room.x][room.y].items, {
+        x = 5,
+        y = 5,
+        representation = 'k',
+        effect = function()
+            logEvent("Found key")
+            messageBox.open({"You found the IRON KEY"})
+            inventory.ironKey = true
+        end
+    })
+    logEvent("Key: F" .. floor .. ", (" .. room.x .. "," .. room.y .. ")")
+
+    if (#dungeon[3].deadEnds == 0) then
+        floor = 4
+    elseif (#dungeon[4].deadEnds == 0) then
+        floor = 3
+    else
+        floor = random(3, 4)
+    end
+    room = table.remove(dungeon[floor].deadEnds)
+    table.insert(dungeon[floor][room.x][room.y].items, {
+        x = 5,
+        y = 5,
+        representation = 'k',
+        effect = function()
+            logEvent("Found key")
+            messageBox.open({"You found the SILVER KEY"})
+            inventory.silverKey = true
+        end
+    })
+    logEvent("Key: F" .. floor .. ", (" .. room.x .. "," .. room.y .. ")")
+
+    if (#dungeon[5].deadEnds == 0) then
+        floor = 6
+    elseif (#dungeon[6].deadEnds == 0) then
+        floor = 5
+    else
+        floor = random(5, 6)
+    end
+    room = table.remove(dungeon[floor].deadEnds)
+    table.insert(dungeon[floor][room.x][room.y].items, {
+        x = 5,
+        y = 5,
+        representation = 'k',
+        effect = function()
+            logEvent("Found key")
+            messageBox.open({"You found the GOLD KEY"})
+            inventory.goldKey = true
+        end
+    })
+    logEvent("Key: F" .. floor .. ", (" .. room.x .. "," .. room.y .. ")")
+
+    --  collect the remaining dead ends and assign life and magic bonuses
+    local deadEnds = {}
+    for floor = 1, FLOORS do
+        for _, deadEnd in pairs(dungeon[floor].deadEnds) do
+            table.insert(deadEnds, {
+                floor = floor,
+                x = deadEnd.x,
+                y = deadEnd.y,
+            })
+        end
+    end
+    scramble(deadEnds)
+    
+    for i = 1, 4 do
+        local room = table.remove(deadEnds)
+        table.insert(dungeon[room.floor][room.x][room.y].items, {
+            x = 5,
+            y = 5,
+            representation = 'l',
+            effect = function()
+                logEvent("Max HP +16")
+                messageBox.open({"You found a life bonus!"})
+                player.maxHp = player.maxHp + 16
+                player.hp = player.hp + 16
+            end
+        })
+        
+        local room = table.remove(deadEnds)
+        table.insert(dungeon[room.floor][room.x][room.y].items, {
+            x = 5,
+            y = 5,
+            representation = 'm',
+            effect = function()
+                logEvent("Max MP +16")
+                messageBox.open({"You found a magic bonus!"})
+                player.maxMp = player.maxMp + 16
+                player.mp = player.mp + 16
+            end
+        })
+    end
+
+    logEvent("Rem. DeadEnds: " .. #deadEnds)
 end
 
