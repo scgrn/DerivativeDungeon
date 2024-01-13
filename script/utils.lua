4471a1e8bee4dbafcd3cbe8b0122d64c67e760ad
Andrew Krause <ajkrause@gmail.com>
2024-01-13 09:29:30 -0500

    Implement scramble

--- script/utils.lua
+++ script/utils.lua
@@ -12,3 +12,14 @@ function string.insert(str1, str2, pos)
     return str1:sub(1, pos) .. str2 .. str1:sub(pos + 1)
 end
 
+--  do the Fisher-Yates shuffle!
+function scramble(a)
+    for i = 1, #a - 1 do
+        local j = random(i, #a)
+
+        local temp = a[i]
+        a[i] = a[j]
+        a[j] = temp
+    end
+end
+
